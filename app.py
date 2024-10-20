7747188a18d152ed75ee4070a4c39976e1c44380
chinmay30mokal <chinmay30mokal@gmail.com>
2024-10-20 20:51:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import json
 import pickle
 
-from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
-app=Flask(__name__)
+app=(__name__)
 ## Load the model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
 scalar=pickle.load(open('scaling.pkl','rb'))
